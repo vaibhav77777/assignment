96db863a87838701d87d8434b6a77a594acfcd2b
vaibhav77777 <33409918+vaibhav77777@users.noreply.github.com>
2020-06-28 22:24:26 +0530

    Add files via upload

---
+++ Default3.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default3.aspx.cs" Inherits="Default3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    </head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:ImageButton ID="ImageButton1" runat="server" Height="88px" ImageUrl="~/sign-in-google.png" OnClick="ImageButton1_Click" Width="188px" />
+            <br />
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default3.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ASPSnippets.GoogleAPI;
+using System.Web.Script.Serialization;
+
+public partial class Default3 :  System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        GoogleConnect.ClientId = "605906953047-gl1ecetvsg7j7itf3tdmjjktsl1ed1e3.apps.googleusercontent.com";
+        GoogleConnect.ClientSecret = "3GfcKUYKDuyHSwMhxalWpjfV";
+        GoogleConnect.RedirectUri = Request.Url.AbsoluteUri.Split('?')[0];
+
+        if (!string.IsNullOrEmpty(Request.QueryString["code"]))
+        {
+            string code = Request.QueryString["code"];
+            string json = GoogleConnect.Fetch("me", code);
+            GoogleProfile profile = new JavaScriptSerializer().Deserialize<GoogleProfile>(json);
+            
+
+         
+        }
+        if (Request.QueryString["error"] == "access_denied")
+        {
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "alert('Access denied.')", true);
+        }
+
+    }
+
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    {
+        GoogleConnect.Authorize("profile", "email");
+
+    }
+    public class GoogleProfile
+    {
+        public string Id { get; set; }
+        public string DisplayName { get; set; }
+        public Image Image { get; set; }
+        public List<Email> Emails { get; set; }
+
+    }
+
+    public class Email
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Image
+    {
+        public string Url { get; set; }
+    }
+
+}
